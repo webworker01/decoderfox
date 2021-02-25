0d871dcc66d0b7143572ace74efbd3f3bd1a5947
webworker01 <webworker@live.com>
2021-02-25 00:30:06 -0500

    Fix generateabi

--- generateabi.js
+++ generateabi.js
@@ -1,19 +1,21 @@
-var solc = require('solc');
-var fs = require('fs');
+const solc = require('solc');
+const fs = require('fs');
 
 // getting the development snapshot
-var solidityVersion = 'v0.6.6+commit.6c089d02';
+const solidityVersion = 'v0.6.6+commit.6c089d02';
 
 solc.loadRemoteVersion(solidityVersion, function(err, solcSnapshot) {
   if (err) {
     // An error was encountered, display and quit
   } else {
-    var contract = fs.readFileSync('contract.sol','utf8').toString();
+    const contractFileName = 'RedFoxMigration.sol';
 
-    var input = {
+    const contract = fs.readFileSync(contractFileName,'utf8').toString();
+
+    let input = {
       language: 'Solidity',
       sources: {
-        'RedFoxMigration.sol': {
+        [contractFileName]: {
           content: contract
         }
       },
@@ -26,16 +28,16 @@ solc.loadRemoteVersion(solidityVersion, function(err, solcSnapshot) {
       }
     };
 
-    var output = JSON.parse(solcSnapshot.compile(JSON.stringify(input)));
+    let output = JSON.parse(solcSnapshot.compile(JSON.stringify(input)));
 
     console.log(output);
 
     // `output` here contains the JSON output as specified in the documentation
-    for (var contractName in output.contracts['RedFoxMigration.sol']) {
+    for (let contractName in output.contracts[contractFileName]) {
       console.log(
         contractName +
           ': ' +
-          output.contracts['RedFoxMigration.sol'][contractName].evm.bytecode.object
+          output.contracts[contractFileName][contractName].evm.bytecode.object
       );
     }
   }
